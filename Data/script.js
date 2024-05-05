e71fe93b7b27daf2e3f797ae6d48a7fc0ee77630
Don-Juan182 <thegoodmarmage@hotmail.com>
2024-05-06 00:19:50 +0200

    Struggling

---
+++ Data/script.js
@@ -0,0 +1,51 @@
+const sports = ["golf", "football", "cricket"];
+
+const games = [
+    {
+        name: "Starcraft 2",
+        released: "2010",        
+    },
+    {
+        name: "Age of Empires II",       
+    },
+    {
+        name: "Cyberpunk 2077",
+        released: 2020
+    }
+];
+//Question1
+
+function printSeason() {
+
+console.log("summer");
+}
+printSeason("season");
+
+//Question2
+
+function printNumbers() {
+
+    console.log("FirstParameter: " + 1);
+    console.log("SecondParameter: " + 2);
+    }
+
+    printNumbers(5, 8);
+
+ //Question3
+
+function addNumbers(thirdParameter, fourthParameter, fifthParameter) {
+
+const result = thirdParameter + fourthParameter + fifthParameter;
+console.log("Total: " + result);
+
+    }
+
+    addNumbers(3, 4, 5);
+
+//Question4
+
+function createGreeting(sixthParameter) {
+
+    const 
+
+}
